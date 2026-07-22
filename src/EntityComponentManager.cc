d4c700876ee11e8f8b4acbd25c229184bd75036e
Luca Della Vedova <lucadv@intrinsic.ai>
2026-07-22 11:55:24 +0800

    Use AddedComponents EnTT tag component to track newly added components

--- src/EntityComponentManager.cc
+++ src/EntityComponentManager.cc
@@ -97,6 +97,10 @@ struct RemovedComponents {
   std::unordered_set<ComponentTypeId> data;
 };
 
+struct AddedComponents {
+  std::unordered_set<ComponentTypeId> data;
+};
+
 class gz::sim::EntityComponentManagerPrivate
 {
   /// \brief Implementation of the CreateEntity function, which takes a specific
@@ -174,16 +178,6 @@ class gz::sim::EntityComponentManagerPrivate
 
   /// \brief Keep track of entities already used to ensure uniqueness.
   public: uint64_t entityCount{0};
-
-  /// \brief Unordered map of added components. The key is the entity to
-  /// which the component belongs, and the value is a set of the component types
-  /// that has been added in this simulation step.
-  /// This is used for tracking of removed components. A component that has
-  /// both been added and removed in this simulation step will not be counted
-  /// as a removed component, since downstream users won't have access to the
-  /// component value in the first place.
-  public: std::unordered_map<Entity, std::unordered_set<ComponentTypeId>>
-    addedComponents;
 
   /// \brief During cloning, we populate two maps:
   ///  - map of cloned model entities to the non-cloned model's canonical link
@@ -281,6 +275,7 @@ EntityComponentManager::EntityComponentManager()
   this->Registry().storage<OneTimeChangedComponents>();
   this->Registry().storage<PeriodicChangedComponents>();
   this->Registry().storage<RemovedComponents>();
+  this->Registry().storage<AddedComponents>();
 
   components::Factory::Instance()->RegisterAllToEntt(this->Registry());
 
@@ -299,9 +294,6 @@ void EntityComponentManagerPrivate::CopyFrom(
     const EntityComponentManagerPrivate &_from)
 {
   this->entityCount = _from.entityCount;
-  this->addedComponents = _from.addedComponents;
-  this->removedComponents = _from.removedComponents;
-  this->componentsMarkedAsRemoved = _from.componentsMarkedAsRemoved;
   // Not copying maps related to cloning since they are transient variables
   // that are used as return values of some member functions.
 }
@@ -609,10 +601,7 @@ void EntityComponentManager::ClearRemovedComponents()
 {
   std::lock_guard<std::mutex> lock(this->dataPtr->removedComponentsMutex);
   this->Registry().clear<RemovedComponents>();
-  this->dataPtr->removedComponents.clear();
-  // Added component map is used purely to avoid false positives in removed
-  // component detection, so we clear it here.
-  this->dataPtr->addedComponents.clear();
+  this->Registry().clear<AddedComponents>();
 }
 
 /////////////////////////////////////////////////
@@ -755,14 +744,12 @@ void EntityComponentManager::PostRemoveComponent(const Entity _entity,
   // Add component to map of removed components
   {
     std::lock_guard<std::mutex> lock(this->dataPtr->removedComponentsMutex);
-    const auto addedCompIt = this->dataPtr->addedComponents.find(_entity);
-    if (addedCompIt == this->dataPtr->addedComponents.end() ||
-        addedCompIt->second.find(_typeId) == addedCompIt->second.end())
+    const auto* addedComp = this->Registry().try_get<AddedComponents>(_entity);
+    if (!addedComp || addedComp->data.find(_typeId) == addedComp->data.end())
     {
       auto& removedComp =
         this->Registry().get_or_emplace<RemovedComponents>(_entity);
       removedComp.data.insert(_typeId);
-      this->dataPtr->removedComponents[_entity].insert(_typeId);
     }
   }
 }
@@ -977,7 +964,9 @@ bool EntityComponentManager::CreateComponentDynamic(
       gzwarn << "Failed syncing component. This should not happen" << std::endl;
     } else {
       updateData = false;
-      this->dataPtr->addedComponents[_entity].insert(_componentTypeId);
+      auto& addedComp =
+        this->Registry().get_or_emplace<AddedComponents>(_entity);
+      addedComp.data.insert(_componentTypeId);
     }
   }
 
